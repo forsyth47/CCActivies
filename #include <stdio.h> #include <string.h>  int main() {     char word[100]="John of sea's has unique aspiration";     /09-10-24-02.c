614dd13705f2a50ee385b582f9a50b0dfc78e5c3
Joshua S <88034696+forsyth47@users.noreply.github.com>
2024-10-09 12:43:20 +0530

    Create  09-10-24-02.c

---
+++ #include <stdio.h> #include <string.h>  int main() {     char word[100]="John of sea's has unique aspiration";     /09-10-24-02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[100]="John of sea's has unique aspiration";
+    //printf("%s", word);
+    
+    char *word = strtok(sentence, " ");
+    printf
+    for (i=0;  ;i++) {
+        
+    }
+
+    return 0;
+}
